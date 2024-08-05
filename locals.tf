88e3341d3e6ef96463147f72e3b11774f51e95e8
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2024-08-04 19:31:24 -0500

    feat: setup vcp

--- locals.tf
+++ locals.tf
@@ -7,9 +7,10 @@ locals {
 
   workspace = {
     "fem-eci-tfe" = {
-      description    = "Example description of workspace"
-      execution_mode = "local"
-      project_id     = module.project["fem-eci-project"].id
+      description         = "Example description of workspace"
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "GiancarlosIO/fem-eci-terraform-tfe"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "giancarlos-isasi"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = 2661096
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "GiancarlosIO"
+  type    = string
+}
